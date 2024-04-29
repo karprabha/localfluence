cdddd828ac4c98a501d00362752c1cf1ea165d8e
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2024-04-30 00:46:16 +0530

    add basic graphQL server setup

---
+++ backend/graphql-server/bin/www.js
@@ -0,0 +1,30 @@
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const createApolloServer = require("../src/app");
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const startServer = async () => {
+  const apolloServer = createApolloServer();
+  const port = normalizePort(process.env.PORT || "4000");
+
+  const { url } = await startStandaloneServer(apolloServer, {
+    listen: { port },
+  });
+
+  console.log(`Server ready at: ${url}`);
+};
+
+startServer();
---
+++ backend/graphql-server/src/app.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("@apollo/server");
+
+const typeDefs = `
+  type Query {
+    root: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    root: () => "Hello World",
+  },
+};
+
+const createApolloServer = () => {
+  return new ApolloServer({
+    resolvers,
+    typeDefs,
+  });
+};
+
+module.exports = createApolloServer;
