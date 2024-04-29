f6fff91f98c707f1411caf7522a692f11b1583b0
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2024-04-29 23:27:43 +0530

    add basic server setup

---
+++ backend/auth-server/bin/www.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const http = require("http");
+const debug = require("debug");
+const app = require("../src/app");
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr?.port}`;
+  debug(`Listening on ${bind}`);
+};
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
