1ba9734bfe3ca5c814023f4818cf945647662ffd
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2024-05-01 23:19:40 +0530

    setup linter and pettier in auth server

--- backend/auth-server/src/app.js
+++ backend/auth-server/src/app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 module.exports = app;
