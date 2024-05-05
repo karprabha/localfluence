cd3ed0c267b5a2620d12b073d93fdfc0fff07ffa
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2024-05-05 15:48:55 +0530

    add morgan logger and express-async-errors

--- backend/auth-server/config/env.config.js
+++ backend/auth-server/config/env.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  POSTGRES_URL: process.env.POSTGRES_URL,
   PORT: process.env.PORT || 3000,
+  POSTGRES_URL: process.env.POSTGRES_URL,
+  MORGAN_MODE: process.env.MORGAN_MODE || 'dev',
 };
--- backend/auth-server/src/app.js
+++ backend/auth-server/src/app.js
@@ -1,6 +1,15 @@
+require('express-async-errors');
+const morgan = require('morgan');
 const express = require('express');
 
+const { MORGAN_MODE } = require('../config');
+
 const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan(MORGAN_MODE));
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
