4d6c22fbbaa5d30e6a20083bf6e2fd016a587a8c
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2024-05-18 03:03:43 +0530

    fix hero img polygon

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="container mx-auto">
         <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-44 xl:pb-44">
           <svg
-            className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 2xl:translate-x-full transform text-white lg:block"
+            className="absolute inset-y-0 right-0 hidden h-full w-44 translate-x-1/2 2xl:translate-x-full transform text-white lg:block"
             fill="currentColor"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
